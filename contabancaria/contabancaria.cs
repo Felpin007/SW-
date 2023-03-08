77b711d4fdb7896725246f6609182e8509d606b3
Felipe <100373987+Felpin007@users.noreply.github.com>
2023-03-08 14:34:25 -0300

    Add files via upload

---
+++ contabancaria/contabancaria.cs
@@ -0,0 +1,18 @@
+namespace contabancaria;
+class Contabancaria
+{
+    public string titular = "";
+    public int saldo;
+
+    public int Sacar(int quanto){
+        return saldo -= quanto;
+    }
+    public int Depositar(int quanto){
+        return saldo += quanto;
+    }
+    public void Exibir(){
+        // Console.Write("O que você quer ver? 1 - titular, 2 - saldo " );
+        Console.Write("O titular da conta é: " + titular + "\n");
+        Console.Write("O saldo atual da conta é:" + saldo + " reais \n");
+    }
+}
